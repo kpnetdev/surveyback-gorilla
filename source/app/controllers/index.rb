7b4881316ab8df30356f83aa07426d7d245d8bb5
David Stavis <davidstavis@gmail.com>
2014-04-11 16:38:21 -0700

    Displays a survey form that creates a new survey and questions when submitted. *comes with corresponding routes in index.rb, helper methods, session logic, and an association with user
    	TODO
    	*separate helper methods

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,19 +1,21 @@
-get '/' do
-  erb :index
-end
-
 get '/surveys/new' do
-	set_page(:create_new_survey)
+	set_page(:create_new_survey_page)
 	erb :index
 end
 
 post '/surveys' do
-	Survey.create(name: params[:survey_name])
-	Survey.questions << Question.create(params)
+	survey = Survey.create(name: params[:survey_name], user_id: session[:user_id])
+	survey.questions << Question.create(params)
+	survey.save
+
 	set_page(:logged_in_home_page)
 	erb :index
 end
 
 def set_page(new_page)
 	session[:page] = new_page
+end
+
+def current_page?(page)
+	session[:page] == page
 end
