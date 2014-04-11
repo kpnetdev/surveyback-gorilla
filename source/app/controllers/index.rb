45fc32340b6167f89752bb4812b3bfa27491f92d
David Stavis <davidstavis@gmail.com>
2014-04-11 15:58:28 -0700

    Has survey creation routes
    TODO:
    	*Survey migration & model

--- source/app/controllers/index.rb
+++ source/app/controllers/index.rb
@@ -1,3 +1,19 @@
 get '/' do
   erb :index
 end
+
+get '/surveys/new' do
+	set_page(:create_new_survey)
+	erb :index
+end
+
+post '/surveys' do
+	Survey.create(name: params[:survey_name])
+	Survey.questions << Question.create(params)
+	set_page(:logged_in_home_page)
+	erb :index
+end
+
+def set_page(new_page)
+	session[:page] = new_page
+end
