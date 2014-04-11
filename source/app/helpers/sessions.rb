ff5a871c9a492d36d93fa9cb62455ee9bd4b0b26
David Stavis <davidstavis@gmail.com>
2014-04-11 16:51:16 -0700

    Has helper methods to set and check current page

--- source/app/helpers/sessions.rb
+++ source/app/helpers/sessions.rb
@@ -4,4 +4,13 @@ helpers do
     # TODO: return the current user if there is a user signed in.
     session[:user] || nil
   end
+
+	def set_page(new_page)
+		session[:page] = new_page
+	end
+
+	def current_page?(page)
+		session[:page] == page
+	end
+	
 end
