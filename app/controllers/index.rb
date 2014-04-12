a55c72a63b8994c1e2bbaf98a966e20d0e8a4c45
kpnetdev <kelley.puckett@gmail.com>
2014-04-11 20:37:44 -0700

    merges bos stuff, blank login page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,9 +3,14 @@ require 'bcrypt'
 
 get '/' do
 	@user = current_user
-	set_page(:logged_in_home_page)
-  erb :index
+	if @user
+		set_page(:logged_in_home_page)
+	  erb :index
+	else
+		erb :login
+	end
 end
+		
 
 get '/surveys/new' do
 	set_page(:create_new_survey_page)
@@ -19,3 +24,30 @@ post '/surveys' do
 	set_page(:logged_in_home_page)
 	erb :index
 end
+
+get '/sessions/new' do
+  erb :sign_in 
+end
+
+post '/sessions' do
+	@user = User.find_by_email(params[:email])
+	  if @user.password == params[:password]
+	    session[:user_id] = @user_id
+	   else
+	   	nil
+	  end
+	redirect '/'
+end
+
+get '/users/new' do
+  erb :sign_up
+end
+
+post '/users' do
+	@user = User.create(params[:user])
+	@user.password = params[:password]
+	p params[:password]
+	session[:user] = @user
+	redirect '/'
+end
+
