dc2a89db4bee97368c018fbd93f7b007e6df88b9
keopaseutb <keopaseutb01@hotmail.com>
2014-04-12 20:28:01 -0700

    Update to master my branch. * Todo:   -make the visualization display.

---
+++ public/js/barchart.js
@@ -0,0 +1,63 @@
+$(function () {
+        $('#container').highcharts({
+            chart: {
+                type: 'bar'
+            },
+            title: {
+                text: 'Survey Answers by Questions'
+            },
+            subtitle: {
+                text: 'Survey Back Gorilla'
+            },
+            xAxis: {
+                categories: ['Survey 1', 'Survey 2', 'Survey 3', 'Survey 4', 'Survey 5'],
+                title: {
+                    text: null
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Answered Questions',
+                    align: 'high'
+                },
+                labels: {
+                    overflow: 'justify'
+                }
+            },
+            tooltip: {
+                valueSuffix: ' response'
+            },
+            plotOptions: {
+                bar: {
+                    dataLabels: {
+                        enabled: true
+                    }
+                }
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'top',
+                x: -40,
+                y: 100,
+                floating: true,
+                borderWidth: 1,
+                backgroundColor: '#FFFFFF',
+                shadow: true
+            },
+            credits: {
+                enabled: false
+            },
+            series: [{
+                name: 'Answer 1',
+                data: [107, 31, 35, 20, 2]
+            }, {
+                name: 'Answer 2',
+                data: [133, 156, 94, 40, 6]
+            }, {
+                name: 'Answer 3',
+                data: [73, 914, 40, 73, 34]
+            }]
+        });
+    });
